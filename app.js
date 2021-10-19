f03502e5a0943b2a398b1ad93972ecdbf84e0641
Ayush Sharma <ayush.sharma@olx.com>
2021-10-19 19:00:05 +0530

    added TransactionEvent listener

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const infuraKey = process.env.INFURA_KEY;
 
 const web3 = new Web3(new Web3.providers.HttpProvider( `https://ropsten.infura.io/v3/${infuraKey}`));
 
-const helloWorld = new web3.eth.Contract([
+const contract = new web3.eth.Contract([
   {
     "constant": true,
     "inputs": [],
@@ -24,6 +24,19 @@ const helloWorld = new web3.eth.Contract([
   }
 ], '0x35c113E1AB11B3001e9085CBafD224FfC3470C67');
 
-helloWorld.methods.output().call({from: '0x8863ae48646c493efF8cd54f9Ffb8Be89669E62A'}, function(error, result) {
+// TODO: use smart contract's address!!
+contract.methods.output().call({from: '0x8863ae48646c493efF8cd54f9Ffb8Be89669E62A'}, function(error, result) {
     console.log(result);
 });
+
+const results = await contract.getPastEvents(
+    'TransactionSent',
+    {
+      filter: {
+        orderPlatform: 'OLX'
+      },
+      fromBlock: 0
+    }
+);
+
+console.log(results);
